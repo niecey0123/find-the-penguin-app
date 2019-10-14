e94e606032d2c26b913a9bf72025a450566b5929
niecey0123 <s.crumpler11@gmail.com>
2019-10-13 22:01:51 -0400

    customizing site with style

---
+++ penguin.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+
+    $(".yeti").mousedown(function() {
+        alert("Yaaaarrrr!");
+    });
+
+    $( ".yeti" ).hide();
+
+    $(".penguin3").click(function(){
+    $(".yeti").show();
+    });
+
+  });
+
+
+
+
+
